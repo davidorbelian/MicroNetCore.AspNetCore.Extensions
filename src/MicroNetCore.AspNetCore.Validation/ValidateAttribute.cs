4c2aeb4ed82a099741cabd898e5fecad994313ed
David Orbelian <david.orbelian@gmail.com>
2017-09-11 14:26:16 +0400

    Add MicroNetCore.AspNetCore.Extensions class library project.

---
+++ src/MicroNetCore.AspNetCore.Validation/ValidateAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MicroNetCore.AspNetCore.ResponseExceptions.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MicroNetCore.AspNetCore.Validation
+{
+    public sealed class ValidateAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid) return;
+
+            context.Result = new BadRequestResult();
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
