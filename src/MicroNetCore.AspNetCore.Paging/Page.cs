d1481f43db5577b0c6d7287efcc347c878f1ec7f
David Orbelian <david.orbelian@gmail.com>
2017-09-11 17:44:41 +0400

    Add MicroNetCore.AspNetCore.Paging class library project.

---
+++ src/MicroNetCore.AspNetCore.Paging/Page.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MicroNetCore.AspNetCore.Paging
+{
+    public sealed class Page<TItem> : List<TItem>
+    {
+        public Page(int pageCount, int pageIndex, int pageSize, IEnumerable<TItem> items)
+            : base(items)
+        {
+            PageCount = pageCount;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+        }
+
+        [DataMember]
+        public int PageCount { get; }
+
+        [DataMember]
+        public int PageIndex { get; }
+
+        [DataMember]
+        public int PageSize { get; }
+    }
+}
