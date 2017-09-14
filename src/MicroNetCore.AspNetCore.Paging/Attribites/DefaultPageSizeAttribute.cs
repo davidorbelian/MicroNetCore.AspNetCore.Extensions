6e252602beab29948ca8a6490927e4da2d5693ac
David Orbelian <david.orbelian@gmail.com>
2017-09-14 20:12:40 +0400

    Add QueryCollectionExtensions for MicroNetCore.AspNetCore.Paging.

---
+++ src/MicroNetCore.AspNetCore.Paging/Attribites/DefaultPageSizeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MicroNetCore.AspNetCore.Paging.Attributes
+{
+    public sealed class DefaultPageSizeAttribute : Attribute
+    {
+        public DefaultPageSizeAttribute(int pageSize)
+        {
+            PageSize = pageSize;
+        }
+
+        public int PageSize { get; }
+    }
+}
---
+++ src/MicroNetCore.AspNetCore.Paging/Extensions/QueryCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Reflection;
+using MicroNetCore.AspNetCore.Paging.Attributes;
+using Microsoft.AspNetCore.Http;
+
+namespace MicroNetCore.AspNetCore.Paging.Extensions
+{
+    public static class QueryCollectionExtensions
+    {
+        public static bool HasPaging(this IQueryCollection query, bool both = false)
+        {
+            var index = int.TryParse(query["pageIndex"].SingleOrDefault(), out var _);
+            var size = int.TryParse(query["pageSize"].SingleOrDefault(), out var _);
+
+            return both
+                ? index && size
+                : index || size;
+        }
+
+        public static int GetPageIndex(this IQueryCollection query)
+        {
+            var index = int.TryParse(query["pageIndex"].SingleOrDefault(), out var pageIndex);
+            return index ? pageIndex : 1;
+        }
+
+        public static int GetPageSize(this IQueryCollection query, int defaultValue = 20)
+        {
+            var size = int.TryParse(query["pageSize"].SingleOrDefault(), out var pageSize);
+            return size ? pageSize : defaultValue;
+        }
+
+        public static int GetPageSize<TModel>(this IQueryCollection query, int defaultValue = 20)
+        {
+            var pageSize = typeof(TModel).GetCustomAttribute<DefaultPageSizeAttribute>()?.PageSize ?? defaultValue;
+            return query.GetPageSize(pageSize);
+        }
+    }
+}
