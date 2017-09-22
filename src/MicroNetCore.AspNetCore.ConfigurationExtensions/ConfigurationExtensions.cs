dab1b9b5501344518835ec22708283d42e270011
David Orbelian <david.orbelian@gmail.com>
2017-09-22 14:22:43 +0400

    Add GetConnectionString extension method for IConfiguration to get the "Default" connection string. Update package version.

---
+++ src/MicroNetCore.AspNetCore.ConfigurationExtensions/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MicroNetCore.AspNetCore.ConfigurationExtensions
+{
+    /// <summary>
+    ///     <see cref="T:Microsoft.Extensions.Configuration.IConfiguration" /> extension methods.
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        ///     Gets "Default" connection string from <see cref="T:Microsoft.Extensions.Configuration.IConfiguration" />.
+        /// </summary>
+        /// <param name="configuration">
+        ///     <see cref="T:Microsoft.Extensions.Configuration.IConfiguration" /> to get the connection
+        ///     string from.
+        /// </param>
+        /// <returns>The "Default" connection string.</returns>
+        public static string GetConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetConnectionString("Default");
+        }
+    }
+}
